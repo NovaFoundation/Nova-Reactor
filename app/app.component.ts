0627fe6479ffe722fb8a7de3b8c3a659724e79ae
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-08 16:14:12 -0800

    Added repo model.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app.component.html',
+  templateUrl: 'app.component.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    repo = {
+        url: undefined
+    }
+}
