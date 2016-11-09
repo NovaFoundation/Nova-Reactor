124f18241b179ec9138848237ffbb6ffc9aa7b9b
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-08 16:20:31 -0800

    Removed HighlightDirective Added UrlValidator

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 // import { routing } from './app.routes';
-import { HighlightDirective } from './validInput.directive';
+import { UrlValidator } from './urlValidator.directive';
 
 @NgModule({
-  imports:      [ BrowserModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, HighlightDirective ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, UrlValidator ],
   bootstrap:    [ AppComponent ]
 })
 
