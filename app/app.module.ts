5064c01517477561dacfafabf88e8378490b7840
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-10 18:29:46 -0800

    Added angular2-oauth2

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }   from '@angular/http';
 
+import { OAuthService} from 'angular2-oauth2/oauth-service';
+
 import { AppComponent }  from './app.component';
 import { GithubUser } from './githubUser.component';
 import { GithubRepo } from './githubRepo.component';
--- systemjs.config.js
+++ systemjs.config.js
@@ -24,6 +24,7 @@
       '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
       // other libraries
       'rxjs':                      'npm:rxjs',
+      'angular2-oauth2':           'node_modules/angular2-oauth2',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
@@ -34,6 +35,21 @@
       },
       rxjs: {
         defaultExtension: 'js'
+      },
+      "angular2-oauth2": {
+          //"baseURL": "/",
+          //"format": "register",
+          "defaultExtension": "js",
+          "map": {
+              "base64-js": "./node_modules/base64-js/lib/b64.js",
+              "buffer": "./node_modules/buffer/index.js",
+              "convert-hex": "./node_modules/convert-hex/convert-hex.js",
+              "convert-string": "./node_modules/convert-string/convert-string.js",
+              "ieee754": "./node_modules/ieee754/index.js",
+              "isarray": "./node_modules/isarray/index.js",
+              "js-base64": "./node_modules/js-base64/base64.js",
+              "sha256": "./node_modules/sha256/lib/sha256.js"
+          },
       }
     }
   });
