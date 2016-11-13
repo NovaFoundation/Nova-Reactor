d0b4f616cb2231d2f35f74c6781af85c3f7e6ba2
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-12 17:37:02 -0800

    Updated to not search for files/commits if repo size is 0.

--- app/githubRepo.component.ts
+++ app/githubRepo.component.ts
@@ -10,16 +10,16 @@ import { GithubService } from './services/github.service';
 export class GithubRepo implements OnChanges {
     @Input() repo: string;
     @Input() username: string;
-    @Output('repo-updated') repoEmitter: EventEmitter<any> = new EventEmitter();
-    @Output('requires-reactor-config') requiresReactorConfigEmitter: EventEmitter<any> = new EventEmitter();
-    @Output('error') errorEmitter: EventEmitter<any> = new EventEmitter();
+    @Output('onRepoUpdated') onRepoUpdated: EventEmitter<any> = new EventEmitter();
+    @Output('onReactorConfigStatus') onReactorConfigStatus: EventEmitter<any> = new EventEmitter();
+    @Output('onError') onError: EventEmitter<any> = new EventEmitter();
     
     model: any;
     commits: any;
     contents: any;
     
     handleError(error: any) {
-        this.errorEmitter.emit({
+        this.onError.emit({
             response: error,
             sender: this
         });
@@ -30,22 +30,38 @@ export class GithubRepo implements OnChanges {
         if (changes.username) this.username = changes.username.currentValue;
         
         if (typeof this.repo === 'string' && typeof this.username === 'string') {
+            this.model = null;
+            this.commits = null;
+            this.contents = null;
+            
             this.github.getRepo(this.username, this.repo).subscribe(repo => {
                 this.model = repo;
                 
-                this.repoEmitter.emit(repo);
-            }, error => this.handleError(error));
-            
-            this.github.getCommits(this.username, this.repo).subscribe(commits => {
-                this.commits = commits.slice(0, Math.min(10, commits.length));
-            }, error => this.handleError(error));
-            
-            this.github.getContents(this.username, this.repo).subscribe(contents => {
-                this.contents = contents;
+                console.log("Received repo ", repo);
                 
-                this.requiresReactorConfigEmitter.emit(this.contents.filter(function (file) {
-                    return file.name.toLowerCase() == ".reactor.yml";
-                }).length == 0);
+                if (repo.size > 0) {
+                    this.github.getCommits(this.username, this.repo).subscribe(commits => {
+                        this.commits = commits.slice(0, Math.min(10, commits.length));
+                        
+                        console.log("Received repo commits ", commits);
+                    }, error => this.handleError(error));
+                    
+                    this.github.getContents(this.username, this.repo).subscribe(contents => {
+                        this.contents = contents;
+                        
+                        this.onReactorConfigStatus.emit(this.contents.filter(function (file) {
+                            return file.name.toLowerCase() == ".reactor.yml";
+                        }).length == 0 ? "requires" : "exists");
+                        
+                        console.log("Received repo contents ", contents);
+                    }, error => this.handleError(error));
+                } else {
+                    console.log("Empty repo");
+                    
+                    this.onReactorConfigStatus.emit("requires");
+                }
+                
+                this.onRepoUpdated.emit(repo);
             }, error => this.handleError(error));
         }
     }
